e9bb6e3c1ae6ed638b99d645e82252dcd1ca7eb1
Rahat Khan Tusar <gb229855@gmail.com>
2021-12-27 15:32:04 +0600

    Style

---
+++ app/AppNavigator.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import HomeScreen from './screens/HomeScreen';
+import MenuScreen from './screens/MenuScreen';
+import DishDetailScreen from './screens/DishDetailScreen';
+import { View,Text} from 'react-native'
+
+
+
+const AppNavigtor = () =>{
+
+    return(       
+          
+            <HomeScreen/>
+             
+         
+    )
+}
+
+export default AppNavigtor;
---
+++ app/screens/DishDetailScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View,Text } from 'react-native'
+
+const DishDetailScreen = () =>{
+
+    return(
+        <View><Text>DishDetailScreen</Text></View>
+    )
+}
+
+export default DishDetailScreen
